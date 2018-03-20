4dc47a5b53711e5f75e2f50270eafe71f1b854c2
maksym <maksym@yelp.com>
2018-03-20 18:30:16 +0100

    move init routines out

--- cmd/ex01.go
+++ cmd/ex01.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"log"
-	"runtime"
-
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 	_ "github.com/go-gl/mathgl/mgl32"
@@ -25,57 +22,17 @@ var (
 )
 
 func main() {
-	runtime.LockOSThread()
+	window := pkg.Init(width, height)
+	defer pkg.Terminate()
 
-	window := initGlfw()
-	defer glfw.Terminate()
+	program := pkg.MakeDefaultProgram()
 
-	program := initOpenGL()
-
-	vao := makeVao(triangle)
+	vbo := pkg.MakeVbo(triangle)
+	vao := pkg.MakeVao(vbo, len(triangle)/3, 0, 0)
 
 	for !window.ShouldClose() {
 		draw(vao, window, program)
 	}
-}
-
-// makeVao initializes and returns a vertex array from the points provided.
-func makeVao(points []float32) uint32 {
-	vbo := pkg.MakeVbo(points)
-	vao := pkg.MakeVao(vbo, len(points)/3, 0, 0)
-	return vao
-}
-
-// initGlfw initializes glfw and returns a Window to use.
-func initGlfw() *glfw.Window {
-	if err := glfw.Init(); err != nil {
-		panic(err)
-	}
-
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-	window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-	window.MakeContextCurrent()
-
-	return window
-}
-
-// initOpenGL initializes OpenGL and returns an intiialized program.
-func initOpenGL() uint32 {
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	log.Println("OpenGL version", version)
-
-	return pkg.MakeDefaultProgram()
 }
 
 func draw(vao uint32, window *glfw.Window, program uint32) {
---
+++ pkg/util.go
@@ -0,0 +1,51 @@
+package pkg
+
+import (
+	"log"
+	"runtime"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func Init(width, height int) *glfw.Window {
+	runtime.LockOSThread()
+
+	window := initGlfw(width, height)
+
+	initOpenGL()
+
+	return window
+}
+
+func Terminate() {
+	glfw.Terminate()
+}
+
+func initGlfw(width, height int) *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "None", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
+}
+
+func initOpenGL() {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+}
